235eac31605ce1ce24b62fc5739796d3d04bb4d9
Razieh Kazeminejad <r.kazeminejad98@gmail.com>
2022-04-24 14:22:05 +0430

    fix delete button bug

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,22 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import './contactList.css';
-import AddContact from '../AddContact/AddContact';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import deleteContact from '../../services/deleteContactService';
 import getContact from '../../services/getContactService';
 
-export default function ContactList({ id }) {
+export default function ContactList({ id1 }) {
   const [contacts, setContacts] = useState(null);
   const [photo, setPhoto] = useState([]);
-  const _getPhoto = async () => {
-    try {
-      const result = await axios.get('http://localhost:3001/contacts');
-      setPhoto(result.data.data);
-    } catch (err) {
-      console.log(err);
-    }
-  };
 
   useEffect(() => {
     _getPhoto();
@@ -28,13 +19,23 @@ export default function ContactList({ id }) {
       getAllContacts();
     } catch (error) {}
   }, []);
-  
+
+  const _getPhoto = async () => {
+    try {
+      const result = await axios.get('http://localhost:3001/contacts');
+      setPhoto(result.data.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
   const deleteContactHandler = async (id) => {
     try {
       await deleteContact(id);
       const filteredContact = contacts.filter((c) => c.id !== id);
       setContacts(filteredContact);
-    } catch (error) {}
+    } catch (error) {
+      console.log('error');
+    }
   };
   return (
     <div className="main">
@@ -42,7 +43,7 @@ export default function ContactList({ id }) {
       <div className="search">
         <input placeholder="search" type="text" name="seatch" />
         <div>
-          <Link to={`/edit/${id}`}>
+          <Link to={`/edit/${id1}`}>
             <button className="uploadBtn">Upload</button>
           </Link>
 
@@ -62,26 +63,32 @@ export default function ContactList({ id }) {
             <th>Phone</th>
             <th></th>
           </tr>
-          {contacts ? contacts.map((contact) => {
-            const { name, avatar, email, phoneNumber, id } = contact;
-            return (
-              <tr key={id}>
-                <td>
-                  <input type="checkbox" />
-                  <img src={avatar} alt="Avatar" />
-                  {name}
-                </td>
-                <td>{email}</td>
-                <td>{phoneNumber}</td>
-                <td>
-                  <Link to={`/edit/${id}`}>
-                    <button>edit</button>
-                  </Link>
-                  <button onDelete={deleteContactHandler}>delete</button>
-                </td>
-              </tr>
-            );
-          }):<p>Loading...</p>}
+          {contacts ? (
+            contacts.map((contact) => {
+              const { name, avatar, email, phoneNumber, id } = contact;
+              return (
+                <tr key={id}>
+                  <td>
+                    <input type="checkbox" />
+                    <img src={avatar} alt="Avatar" />
+                    {name}
+                  </td>
+                  <td>{email}</td>
+                  <td>{phoneNumber}</td>
+                  <td>
+                    <Link to={`/edit/${id}`}>
+                      <button>edit</button>
+                    </Link>
+                    <button onClick={() => deleteContactHandler(id)}>
+                      delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            })
+          ) : (
+            <p>Loading...</p>
+          )}
         </table>
       </div>
     </div>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,20 +1,14 @@
-import { useState, useEffect } from 'react';
 import ContactList from '../components/ContactList/ContactList';
 import Header from '../components/Header/Header';
 import SideBar from '../components/SideBar/SideBar';
-import getContact from '../services/getContactService';
-import deleteContact from '../services/deleteContactService';
-
 
 export default function HomePage() {
-
   return (
     <div>
       <Header />
       <div className="mainPage">
         <SideBar />
-        <ContactList
-        />
+        <ContactList />
       </div>
     </div>
   );
